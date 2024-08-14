3536c822058c5d310c476d6b3a3408e9d2811a5a
Salma-Moh <salmamoh168@gmail.com>
2024-08-14 08:36:27 +0300

    Initial commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,24 @@
 // src/components/Home.js
 import React from 'react';
+import './css/home.css';
+import logo from '../logo.svg';
 
-function Home() {
+function Home({ articleTitle }) {
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            {/* Add more content here */}
+        <div className="home">
+            {articleTitle === 'first' ? (
+                <div className="article">
+                    <h2>Article Title: {articleTitle}</h2>
+                    <img src={logo} alt="Logo" />
+                </div>
+            ) : articleTitle === 'second' ? (
+                <div className="article">
+                    <h2>Article Title: {articleTitle}</h2>
+                    <p>This is a paragraph for the article with the title "second".</p>
+                </div>
+            ) : (
+                <p>No article to display.</p>
+            )}
         </div>
     );
 }
